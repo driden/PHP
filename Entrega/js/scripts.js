4540929d3506d85dc9eb7ddded130293f0b3af4d
driden <driden_87@hotmail.com>
2018-02-10 18:08:16 -0300

    scripts locos

--- Entrega/js/scripts.js
+++ Entrega/js/scripts.js
@@ -1,9 +1,25 @@
 
-$(document).ready(function() {
-    $('p.description').each(function(){
+$(document).ready(function () {
+    $('p.description').each(function () {
         let $this = $(this)
         const text = $this.text()
-        $this.text(text.substr(0,150)+'...')
-    }   
-)})
+        $this.text(text.substr(0, 150) + '...')
+    }
+    )
+})
+
+function scrollFilters() {
+    const offset = 0; // lo usamos depsues de que pasa el header
+    const element = document.getElementById('filters');
+    const yOffset = window.pageYOffset;
+ 
+    if (yOffset > 160 && yOffset < 1400) {// altura del header
+        let pixels = parseInt((window.pageYOffset + offset)) 
+        element.style.top = pixels+ 'px';
+        console.log(pixels);
+    }
+    else element.style.top = '160px';
+}
+
+window.setInterval(scrollFilters, 0);
 
