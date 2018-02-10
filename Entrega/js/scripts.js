58695af3ce7d3f2696bd36e02e701d558bf909cc
driden <776982+driden@users.noreply.github.com>
2018-02-10 13:32:30 -0300

    Tarjeta perrito

--- Entrega/js/scripts.js
+++ Entrega/js/scripts.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
         let $this = $(this)
         const text = $this.text()
         $this.text(text.substr(0,150)+'...')
-        $this.prev().css('margin-bottom',0)
     }   
 )})
 
