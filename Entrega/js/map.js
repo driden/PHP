32b259b7f4afbc34175ca4c94a80c9e89ed23a60
driden <776982+driden@users.noreply.github.com>
2018-02-17 13:44:37 -0300

    GOOGLE MAPS YEAHH

--- Entrega/js/map.js
+++ Entrega/js/map.js
@@ -13,6 +13,19 @@ function initMap() {
       map: map
     });
   }
+
+  function initMapPlaceableMarker() {
+    const uluru = {
+        lat: -34.890357, 
+        lng: -56.106040099999994
+    };
+    
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: uluru
+    });
+
+  }
 /*
   function initMap(lat, long) {
     const uluru = {lat: lat, lng: long};
