4dadc276346720a0e3eed33a1d33301f22685e77
jpgonzalez87 <jp.gonzalezchalar@gmail.com>
2018-03-03 11:14:27 -0300

    mensajes de error  registro funcionando OKA.

--- Entrega/js/clientValidations.js
+++ Entrega/js/clientValidations.js
@@ -1,33 +1,4 @@
-$(document).ready(function() {
-   /*lo saque de aca: https://codepen.io/fmobiledesigns/pen/wMjYOm */
 
-
-   
-   
-    $('#user-form').validate({
-   rules: {
-     email: {
-       required: true,
-       email: true
-     },
-   },
-   
-   highlight: function(element, errorClass, validClass) { 
-     $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-ok').addClass('glyphicon-remove');
-     $(element).addClass(errorClass).removeClass(validClass);
-     $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
-   },
-   success: function(element) {
-     $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-remove').addClass('glyphicon-ok');
-  element.closest('.form-group').removeClass('has-error').addClass('has-success');
-     $(element).remove();
-   }
- });
-});
-
-
-
-/*
 $(document).ready(function () {
 
        let component = {
@@ -79,4 +50,4 @@ $(document).ready(function () {
 
 
 
-})*/
+})
