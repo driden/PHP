7ed13ed6d86fe757b1dc08274ef688727b18951b
jpgonzalez87 <jp.gonzalezchalar@gmail.com>
2018-03-07 21:03:41 -0300

    Validacion usuario unico en register

--- Entrega/Netbeans/dataRegister.php
+++ Entrega/Netbeans/dataRegister.php
@@ -1,22 +1,43 @@
 <?php
 
 require_once 'datos.php';
+
 $email = $_POST ["email"];
-$name = $_POST ["fullName"];
-$password = $_POST ["password"];
-$cn = getConexion();
-$cn->consulta(
-        "insert into usuarios"
-        . "(email, nombre, password)"
-        . " values (:email, :name, :password)", array(
-    array("email", $email, 'string'),
-    array("name", $name, 'string'),
-    array("password", $password, 'string')
-));
+
+$userExists = "select * from usuarios where email = :mail";
+$cnValidate = getConexion();
+
+$cnValidate->consulta($userExists, [["mail", $email, 'string']]);
+$result = $cnValidate->restantesRegistros();
+echo $cnValidate->ultimoError();
+$cnValidate->desconectar();
+
+if (count($result) == 0) {
+
+
+    $name = $_POST ["fullName"];
+    $password = $_POST ["password"];
+    $cn = getConexion();
+    $cn->consulta(
+            "insert into usuarios"
+            . "(email, nombre, password)"
+            . " values (:email, :name, :password)", array(
+        array("email", $email, 'string'),
+        array("name", $name, 'string'),
+        array("password", $password, 'string')
+    ));
+    
+
+    if ($cn->ultimoIdInsert() > 0) {
+        header('location: index.php');
+    } else {
+        echo $cn->ultimoError();
+    }
+} else {
+    echo 'Usuario ya registrado!';
+}
 
 //To-do: guardar sesion
-if($cn->ultimoIdInsert() > 0) {
-    header('location: index.php');
-} else {
-    echo $cn->ultimoError();
-}
+//getSessionUser();
+
+
